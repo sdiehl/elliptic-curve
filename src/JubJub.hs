82fd33c51a0f7b8cab3253f24b1659e49b945b95
David Kurniadi Angdinata <david@adjoint.io>
2019-06-10 17:45:56 +0100

    Add functions on points in affine coordinates

--- src/JubJub.hs
+++ src/JubJub.hs
@@ -19,3 +19,35 @@ _h = 8
 
 _d :: Rational
 _d = -(10240/10241)
+
+-- | Point in affine coordinates
+data PointA = PointA Rational Rational deriving Eq
+
+-- | Affine addition formula
+affineAdd :: PointA -> PointA -> PointA
+affineAdd p1@(PointA x1 y1) p2@(PointA x2 y2)
+  | p1 == p2  = affineDouble p1
+  | otherwise = PointA x3 y3
+  where
+    x1x2 = x1*x2
+    y1y2 = y1*y2
+    x1y2 = x1*y2
+    x2y1 = x2*y1
+    xxyy = x1x2*y1y2
+    x3   = (x1y2+x2y1)/(1+_d*xxyy)
+    y3   = (y1y2+x1x2)/(1-_d*x1x2*y1y2)
+
+-- | Affine doubling formula
+affineDouble :: PointA -> PointA
+affineDouble (PointA x1 y1) = PointA x3 y3
+  where
+    x1x1 = x1*x1
+    x1y1 = x1*y1
+    y1y1 = y1*y1
+    xxyy = x1x1*y1y1
+    x3   = (x1y1+x1y1)/(1+_d*xxyy)
+    y3   = (y1y1+x1x1)/(1-_d*xxyy)
+
+-- | Affine negation formula
+affineNegate :: PointA -> PointA
+affineNegate (PointA x y) = PointA (-x) y
