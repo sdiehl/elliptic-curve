d3c272b4b5a8ac7ead62c0d2b134e25860b7ba74
Multramate <dka316@ic.ac.uk>
2019-07-11 11:53:51 +0100

    Rearrange tests and benchmarks layout

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -86,76 +86,67 @@ import qualified Curve.Weierstrass.SECP521R1       as SECP521R1
 import GHC.Base
 
 benchmark :: forall r c k . Curve r c k => String -> Point r c k -> Benchmark
-benchmark curve point = let point' = double point in bgroup curve
-  [ bench "Addition"
-    $ whnf (uncurry (<>)) (point, point')
-  , bench "Doubling"
-    $ whnf double point
-  , bench "Inversion"
-    $ whnf inv point
-  , bench "Multiplication"
-    $ whnf (mul 6) point
-  ]
+benchmark = (. whnf (mul 6)) . bench
 
 main :: IO ()
 main = defaultMain
   [ bgroup "Binary"
-    [ benchmark "SECT113R1" SECT113R1._g
-    , benchmark "SECT113R2" SECT113R2._g
-    , benchmark "SECT131R1" SECT131R1._g
-    , benchmark "SECT131R2" SECT131R2._g
-    , benchmark "SECT163K1" SECT163K1._g
-    , benchmark "SECT163R1" SECT163R1._g
-    , benchmark "SECT163R2" SECT163R2._g
-    , benchmark "SECT193R1" SECT193R1._g
-    , benchmark "SECT193R2" SECT193R2._g
-    , benchmark "SECT233K1" SECT233K1._g
-    , benchmark "SECT233R1" SECT233R1._g
-    , benchmark "SECT239K1" SECT239K1._g
-    , benchmark "SECT283K1" SECT283K1._g
-    , benchmark "SECT283R1" SECT283R1._g
-    , benchmark "SECT409K1" SECT409K1._g
-    , benchmark "SECT409R1" SECT409R1._g
-    , benchmark "SECT571K1" SECT571K1._g
-    , benchmark "SECT571R1" SECT571R1._g
+    [ benchmark        "SECT113R1"       SECT113R1._g
+    , benchmark        "SECT113R2"       SECT113R2._g
+    , benchmark        "SECT131R1"       SECT131R1._g
+    , benchmark        "SECT131R2"       SECT131R2._g
+    , benchmark        "SECT163K1"       SECT163K1._g
+    , benchmark        "SECT163R1"       SECT163R1._g
+    , benchmark        "SECT163R2"       SECT163R2._g
+    , benchmark        "SECT193R1"       SECT193R1._g
+    , benchmark        "SECT193R2"       SECT193R2._g
+    , benchmark        "SECT233K1"       SECT233K1._g
+    , benchmark        "SECT233R1"       SECT233R1._g
+    , benchmark        "SECT239K1"       SECT239K1._g
+    , benchmark        "SECT283K1"       SECT283K1._g
+    , benchmark        "SECT283R1"       SECT283R1._g
+    , benchmark        "SECT409K1"       SECT409K1._g
+    , benchmark        "SECT409R1"       SECT409R1._g
+    , benchmark        "SECT571K1"       SECT571K1._g
+    , benchmark        "SECT571R1"       SECT571R1._g
     ]
   , bgroup "Edwards"
-    [ benchmark "Curve1174" Curve1174._g
-    , benchmark "Curve41417" Curve41417._g
-    , benchmark "E-222" E222._g
-    , benchmark "E-382" E382._g
-    , benchmark "E-521" E521._g
-    , benchmark "Ed448" Ed448._g
-    , benchmark "Ed3363" Ed3363._g
-    , benchmark "Ed25519" Ed25519._g
-    , benchmark "JubJub" JubJub._g
+    [ benchmark        "Curve1174"       Curve1174._g
+    , benchmark       "Curve41417"      Curve41417._g
+    , benchmark            "E-222"            E222._g
+    , benchmark            "E-382"            E382._g
+    , benchmark            "E-521"            E521._g
+    , benchmark            "Ed448"           Ed448._g
+    , benchmark           "Ed3363"          Ed3363._g
+    , benchmark          "Ed25519"         Ed25519._g
+    , benchmark           "JubJub"          JubJub._g
     ]
   , bgroup "Montgomery"
-    [ benchmark "Curve448" Curve448._g
-    , benchmark "Curve25519" Curve25519._g
-    , benchmark "Curve383187" Curve383187._g
-    , benchmark "M-221" M221._g
-    , benchmark "M-383" M383._g
-    , benchmark "M-511" M511._g
+    [ benchmark         "Curve448"        Curve448._g
+    , benchmark       "Curve25519"      Curve25519._g
+    , benchmark      "Curve383187"     Curve383187._g
+    , benchmark            "M-221"            M221._g
+    , benchmark            "M-383"            M383._g
+    , benchmark            "M-511"            M511._g
     ]
   , bgroup "Weierstrass"
-    [ benchmark "ANSSI-FRP256V1" ANSSIFRP256V1._g
-    , benchmark "BLS12-381" BLS12_381._g
-    , benchmark "BLS12-381T" BLS12_381T._g
-    , benchmark "BLS48-581" BLS48_581._g
-    , benchmark "BLS48-581T" BLS48_581T._g
-    , benchmark "BN224" BN224._g
-    , benchmark "BN254" BN254._g
-    , benchmark "BN254T" BN254T._g
-    , benchmark "BN254A" BN254A._g
-    , benchmark "BN254AT" BN254AT._g
-    , benchmark "BN254B" BN254B._g
-    , benchmark "BN254BT" BN254BT._g
-    , benchmark "BN256" BN256._g
-    , benchmark "BN384" BN384._g
-    , benchmark "BN462" BN462._g
-    , benchmark "BN462T" BN462T._g
-    , benchmark "BN512" BN512._g
+    [ benchmark   "ANSSI-FRP256V1"   ANSSIFRP256V1._g
+    , benchmark        "BLS12-381"       BLS12_381._g
+    , benchmark       "BLS12-381T"      BLS12_381T._g
+    , benchmark        "BLS48-581"       BLS48_581._g
+    , benchmark       "BLS48-581T"      BLS48_581T._g
+    , benchmark            "BN224"           BN224._g
+    , benchmark            "BN254"           BN254._g
+    , benchmark           "BN254T"          BN254T._g
+    , benchmark           "BN254A"          BN254A._g
+    , benchmark          "BN254AT"         BN254AT._g
+    , benchmark           "BN254B"          BN254B._g
+    , benchmark          "BN254BT"         BN254BT._g
+    , benchmark            "BN256"           BN256._g
+    , benchmark            "BN384"           BN384._g
+    , benchmark            "BN462"           BN462._g
+    , benchmark           "BN462T"          BN462T._g
+    , benchmark            "BN512"           BN512._g
     , benchmark "Brainpool-P160R1" BrainpoolP160R1._g
     , benchmark "Brainpool-P160T1" BrainpoolP160T1._g
     , benchmark "Brainpool-P192R1" BrainpoolP192R1._g
@@ -170,20 +161,20 @@ main = defaultMain
     , benchmark "Brainpool-P384T1" BrainpoolP384T1._g
     , benchmark "Brainpool-P512R1" BrainpoolP512R1._g
     , benchmark "Brainpool-P512T1" BrainpoolP512T1._g
-    , benchmark "SECP112R1" SECP112R1._g
-    , benchmark "SECP112R2" SECP112R2._g
-    , benchmark "SECP128R1" SECP128R1._g
-    , benchmark "SECP128R2" SECP128R2._g
-    , benchmark "SECP160K1" SECP160K1._g
-    , benchmark "SECP160R1" SECP160R1._g
-    , benchmark "SECP160R2" SECP160R2._g
-    , benchmark "SECP192K1" SECP192K1._g
-    , benchmark "SECP192R1" SECP192R1._g
-    , benchmark "SECP224K1" SECP224K1._g
-    , benchmark "SECP224R1" SECP224R1._g
-    , benchmark "SECP256K1" SECP256K1._g
-    , benchmark "SECP256R1" SECP256R1._g
-    , benchmark "SECP384R1" SECP384R1._g
-    , benchmark "SECP521R1" SECP521R1._g
+    , benchmark        "SECP112R1"       SECP112R1._g
+    , benchmark        "SECP112R2"       SECP112R2._g
+    , benchmark        "SECP128R1"       SECP128R1._g
+    , benchmark        "SECP128R2"       SECP128R2._g
+    , benchmark        "SECP160K1"       SECP160K1._g
+    , benchmark        "SECP160R1"       SECP160R1._g
+    , benchmark        "SECP160R2"       SECP160R2._g
+    , benchmark        "SECP192K1"       SECP192K1._g
+    , benchmark        "SECP192R1"       SECP192R1._g
+    , benchmark        "SECP224K1"       SECP224K1._g
+    , benchmark        "SECP224R1"       SECP224R1._g
+    , benchmark        "SECP256K1"       SECP256K1._g
+    , benchmark        "SECP256R1"       SECP256R1._g
+    , benchmark        "SECP384R1"       SECP384R1._g
+    , benchmark        "SECP521R1"       SECP521R1._g
     ]
   ]
